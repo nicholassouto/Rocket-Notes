24fa1aca5a3a7f2560f549bc0279a8d00ee0c4e6
Nicholas <nssouto@hotmail.com>
2023-06-22 10:28:43 -0300

    component adjustment and server changed

--- src/components/Note/index.jsx
+++ src/components/Note/index.jsx
@@ -2,12 +2,13 @@ import { Container } from "./style";
 import { Tag } from "../../components/Tags";
 
 export function Note({ data, ...rest }) {
+  console.log(data);
   return (
     <Container {...rest}>
       <h1>{data.title}</h1>
-      {data.tags && (
+      {data.tag && (
         <footer>
-          {data.tags.map((tag) => (
+          {data.tag.map((tag) => (
             <Tag key={tag.id} title={tag.name} />
           ))}
         </footer>
